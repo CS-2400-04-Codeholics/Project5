81108dab7e678bd8430540adff6d73509b09f3e5
Miguel Sierra <98719795+UndeadBatman@users.noreply.github.com>
2022-05-08 16:01:58 -0700

    Add files via upload

--- Bag.java
+++ Bag.java
@@ -27,5 +27,83 @@ public class Bag<T>
             throw new IllegalStateException("Bag exceeds max capacity.");
         }
     }
-    
+
+    private void checkIntegrity()
+    {
+        if(integrityCheck == false)
+        {
+            throw new SecurityException("Invalid Input");
+        }
+    }
+
+    private void doubleCapacity()
+    {
+        int newLength = 2 * bag.length;
+        checkCapacity(newLength);
+        bag = Arrays.copyOf(bag, newLength);
+    }
+
+    private void checkCapacity(int capacity)
+    {
+        if(capacity > maxCapacity)
+        {
+            throw new IllegalStateException("Bag is larger than the max capacity.");
+        }
+    }
+
+    public boolean isFull()
+    {
+        return entryNumbers == bag.length;
+    }
+
+    public boolean isEmpty()
+    {
+        return entryNumbers == 0;
+    }
+
+    public int getSize()
+    {
+        return entryNumbers;
+    }
+
+    public T[] toArray()
+    {
+        T[] result = (T[])new Object[entryNumbers];
+        for(int index = 0; index < entryNumbers; index++)
+        {
+            result[index] = bag[index];
+        }
+        return result;
+    }
+
+    public int getFrequencyOf(T anEntry)
+    {
+        checkIntegrity();
+        int count = 0;
+        for(int index = 0; index < entryNumbers; index++)
+        {
+            if(anEntry.equals(bag[index]))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean add(T newEntry)
+    {
+        checkIntegrity();
+        boolean result = true;
+        if(isFull())
+        {
+            doubleCapacity();
+        }
+        else
+        {
+            bag[entryNumbers] = newEntry;
+            entryNumbers++;
+        }
+        return result;
+    }
+
 }
