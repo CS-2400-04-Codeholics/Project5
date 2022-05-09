da93ecd29226b4f48096aa854f9e686ca0bd96af
Miguel Sierra <98719795+UndeadBatman@users.noreply.github.com>
2022-05-08 18:50:11 -0700

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -30,15 +30,52 @@ public class Driver
 		graph.addEdge(8, 5);
 
         System.out.print("Breadth-First Traversal: ");
-
+        getBreadthFirstTraversal(0, graph);
         System.out.println();
-
-        System.out.print("Depth-First Traversal: ");
-
-        System.out.println();
-
 
     }
 
-    
+    /**
+     * Task 1: Write a program that uses an algorithm that performs a breadth-first traversal.
+     * @param origin
+     * @param graph
+     */
+    public static void getBreadthFirstTraversal(int origin, Graph<String> graph)
+    {
+        Bag<Integer> TraversalOrder = new Bag<>();
+        TraversalOrder.add(origin);
+        System.out.print(graph.getLabel(origin));
+
+        int[] queue = new int[10];
+        int queCount = 0;
+        boolean count = false;
+        int[] neighbors = graph.neighbors(origin);
+        for(int i = 0; i < neighbors.length; i++)
+        {
+            queue[i] = neighbors[i];
+            queCount++;
+            TraversalOrder.add(neighbors[i]);
+            System.out.print(graph.getLabel(neighbors[i]));
+        }
+        while(queCount != 0)
+        {
+            neighbors = graph.neighbors(queue[0]);
+            for(int i = 0; i < neighbors.length; i++)
+            {
+                if(!TraversalOrder.contains(neighbors[i]))
+                {
+                    queue[queCount] = neighbors[i];
+                    queCount++;
+                    count = true;
+                    TraversalOrder.add(neighbors[i]);
+                    System.out.print(graph.getLabel(neighbors[i]));               
+                }
+            }
+            for(int i = 0; i < queue.length - 1; i++)
+                queue[i] = queue[i + 1];
+            if(count == true)
+                queCount--;
+        }
+    }
+
 }
